5a427c5d80dd00ec8f2cc24019abaa72b00039d7
daniel <daniel.suman@student.ossp.cz>
2021-09-17 11:55:19 +0200

    přidán soubor

---
+++ carshop.sql
@@ -0,0 +1,9 @@
+-- Database creation
+CREATE DATABASE database_name;
+
+-- Table creation
+CREATE TABLE car ( 
+ id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+ manufacturer VARCHAR(255), 
+ model VARCHAR(255)
+)
