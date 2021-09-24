92fa34d84b4e4d9d33cd4b749d78097793de3435
daniel <daniel.suman@student.ossp.cz>
2021-09-24 13:25:15 +0200

    I gave added the 20x insert

--- carshop.sql
+++ carshop.sql
@@ -25,3 +25,27 @@ FROM car;
 
 --Drop
 DROP TABLE car;
+
+--Insert into 20x
+INSERT INTO car (model, manufacturer)
+VALUES 
+('Ford Focus', 'Ford'),
+('125S', 'FERRARI '), 
+('W31','Mercedes Benz'),
+('Bugatti Veyron','Volkswagen Group'),
+('Huracán Evo','Lamborghini'),
+('Fiat 126','Fiat'),
+('¨Fabia Combi','Škoda Auto'),
+('DeLorean DMC-12','DeLorean Motor Company'),
+('3/15','BMW Group'),
+('Cybertruck','Tesla')
+('Dodge Charger','Stellantis'),
+('GMC Acadia','General Motors'),
+('Infiniti','Renault-Nissan-Mitsubishi Alliance'),
+('Lexus ES','Toyota Motor Corp'),
+('Volvo S60',' Zhejiang Geely Holding Group'),
+('Saab 9-2X','Saab AB'),
+('ARNAGE T','Bentley Motors'),
+('Nissan 350Z','Nissan'),
+('718 Cayman','Porsche'),
+('Subaru Outback','Subaru');
