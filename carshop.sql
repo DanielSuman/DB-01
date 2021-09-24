abee3b1ccf399b04207c533f950ae2cffa9ca8d3
daniel <daniel.suman@student.ossp.cz>
2021-09-24 11:54:27 +0200

    made changes O_o

--- carshop.sql
+++ carshop.sql
@@ -7,3 +7,21 @@ CREATE TABLE car (
  manufacturer VARCHAR(255), 
  model VARCHAR(255)
 )
+
+-- Describe
+DESCRIBE car;
+
+-- Insert into
+INSERT INTO car (id, model, manufacturer)
+VALUES (1, 'i20', 'Hyundai');
+
+--Insert into auto_increment
+INSERT INTO car (model, manufacturer)
+VALUES ('i20', 'Hyundai');
+
+--Select
+SELECT *
+FROM car;
+
+--Drop
+DROP TABLE car;
