fef81ac99ab3aa9e7a87b00836bc38b02e9c8b37
daniel <daniel.suman@student.ossp.cz>
2021-10-07 22:37:58 +0200

    I have finally done this thing

--- carshop.sql
+++ carshop.sql
@@ -70,3 +70,26 @@ phone INT,
 birthdate INT,
 description TEXT
 );
+
+-- Insert into Garage
+INSERT INTO garage (capacity, gps_x, gps_y, name, description)
+VALUES 
+(4, 200, 200, 'Stanoviště A', 'První Garáž'),
+(8, 400, 400, 'Stanoviště B', 'Druhá Garáž'),
+(12, 600, 600, 'Stanoviště C', 'Třetí Garáž'),
+(16, 800, 800, 'Stanoviště D', 'Čtvrtá Garáž'),
+(20, 1000, 1000, 'Stanoviště E', 'Pátá Garáž');
+
+-- Insert into Employee
+INSERT INTO employee (firstname, lastname, email, phone, birthdate, description)
+VALUES 
+('Jaroslav', 'Kašák', 'jaroslav.kasak@student.ossp.cz', 999666333, 12022004, 'nerd'),
+('Radek', 'Starý', 'radek.stary@gmail.com', 555888222, 01011990, 'youtuber'),
+('Martin', 'Kokeš', 'martin.kokes@ossp.cz', 568994156, 04071996, 'ucitel'),
+('Rudolf', 'von Stroheim', 'rudolfvonstroheim@ssmail.de', 000000000, 05011900, 'hrdy nemecky vojak, ktery padnul v Bitve o Stalingrad'),
+('Gordon', 'Freeman', 'gordon.freeman@scienceteam.blackmesa.us', 205654789, 19721982, 'němý vědátor'),
+('Felix', 'Kjellberg', 'pewdiepie@gmail.com', 106208405, 24101989, '#1 Na YouTube'),
+('Sven','Austerlitz', 'sven.austerlitz@gmail.com', 08295785, 1271958, 'Náhodný němec'),
+('Honza', 'Pilař', 'honza.pilar@student.ossp.cz', 741852963, 2151990, 'idk'),
+('Peter', 'Parker', 'peterparker@hotmail.com', 444222888, 555549, 'Tvůj hodný soused spiderman'),
+('Bob', 'Ross', 'bobross.painter@gmail.com', 55566622, 02141500, 'Proste malir');
